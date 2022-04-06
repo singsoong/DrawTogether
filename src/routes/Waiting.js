05a5e0302329f429fc313b9167ac0e6e418e549c
Hyukjae Ryu <o__hyeah@naver.com>
2022-04-06 22:43:44 +0900

    waiting 화면설계

--- src/routes/Waiting.js
+++ src/routes/Waiting.js
@@ -3,9 +3,24 @@ import styles from "../css/Waiting.module.css";
 function Waiting() {
   return (
     <>
-      <div>
+      <div className={styles.container}>
         <div className={styles.header}>
           <button className={styles.settingBtn}>설정</button>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>같이 그릴까?</h1>
+          <h4 className={styles.code}>입장 코드 : 123456</h4>
+        </div>
+        <div className={styles.playerContainer}>
+          <div className={styles.player}>
+            <div className={styles.startBtn}>시작하기</div>
+            <div className={styles.readyBtn}>준비하기</div>
+          </div>
+          <div className={styles.player}>1</div>
+          <div className={styles.player}>2</div>
+          <div className={styles.player}>3</div>
+          <div className={styles.player}>4</div>
+          <div className={styles.player}>5</div>
         </div>
       </div>
     </>
