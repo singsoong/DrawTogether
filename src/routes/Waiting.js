03774b2f5147166f6f692e65768a1820bd943168
Singsoong <o__hyeah@naver.com>
2022-04-20 02:15:06 +0900

    Waiting.js styled components 적용

--- src/routes/Waiting.js
+++ src/routes/Waiting.js
@@ -1,33 +1,79 @@
-import { useState } from "react";
-import styles from "../css/Waiting.module.css";
 import styled from "styled-components";
 import SettingBtn from "../components/SettingBtn";
+
+const Container = styled.div`
+  padding-left: 10vw;
+`;
+
+const Header = styled.div`
+  height: 10vh;
+`;
+
+const Content = styled.div`
+  padding-left: 10vw;
+  height: 20vh;
+`;
+
+const Title = styled.h1`
+  display: inline;
+`;
+
+const CodeText = styled.h4`
+  margin-left: 10vw;
+  display: inline;
+`;
+
+const PlayerContainer = styled.div`
+  display: grid;
+  grid-template-columns: 25vw 25vw 25vw;
+  grid-template-rows: 25vh 25vh;
+  grid-gap: 3vw;
+  text-align: center;
+`;
+
+const Player = styled.div`
+  padding-left: 3vw;
+  padding-right: 3vw;
+  padding-top: 3vh;
+  border: 1px solid #718093;
+`;
+
+const StartBtn = styled.div`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  display: block;
+  padding: 10px 60px;
+  border: 1px solid #718093;
+  cursor: pointer;
+`;
+
+const ReadyBtn = styled(StartBtn)``;
 
 function Waiting() {
   const enterCode = window.location.pathname.slice(-6);
 
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.header}>
+      <Container>
+        <Header>
           <SettingBtn>설정</SettingBtn>
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>같이 그릴까?</h1>
-          <h4 className={styles.code}>입장 코드 : {enterCode}</h4>
-        </div>
-        <div className={styles.playerContainer}>
-          <div className={styles.player}>
-            <div className={styles.startBtn}>시작하기</div>
-            <div className={styles.readyBtn}>준비하기</div>
-          </div>
-          <div className={styles.player}>1</div>
-          <div className={styles.player}>2</div>
-          <div className={styles.player}>3</div>
-          <div className={styles.player}>4</div>
-          <div className={styles.player}>5</div>
-        </div>
-      </div>
+        </Header>
+        <Content>
+          <Title>같이 그릴까?</Title>
+          <CodeText>입장 코드 : {enterCode}</CodeText>
+        </Content>
+        <PlayerContainer>
+          <Player>
+            <StartBtn>시작하기</StartBtn>
+            <ReadyBtn>준비하기</ReadyBtn>
+          </Player>
+          <Player>1</Player>
+          <Player>2</Player>
+          <Player>3</Player>
+          <Player>4</Player>
+          <Player>5</Player>
+        </PlayerContainer>
+      </Container>
     </>
   );
 }
