b46f441d6fc9a373699ca8194b0efc608d8322d2
DTchme <72367405+DTchme@users.noreply.github.com>
2022-05-13 15:58:26 +0900

    5초후 실행

--- src/routes/Waiting.js
+++ src/routes/Waiting.js
@@ -3,7 +3,7 @@ import SettingBtn from "../components/SettingBtn";
 import axios from "axios";
 import React, { useEffect, useReducer, useRef, useState } from "react";
 import { useHistory } from "react-router-dom";
-import {socket} from "../etc/Socket";
+import { socket } from "../etc/Socket";
 
 const Container = styled.div`
   padding-left: 10vw;
@@ -80,7 +80,7 @@ const Waiting = (props) => {
   const [color5, setColor5] = useState("white");
   const [myposition, setpMyposition] = useState(-1);
 
-  const startClick = () => {
+  const start = () => {
     history.push("/game");
   };
 
@@ -111,13 +111,13 @@ const Waiting = (props) => {
   };
 
   // user 상태 세팅 함수
-  const SettingState = (data)=>{
+  const SettingState = (data) => {
     setp1state(data.p1.state);
     setp2state(data.p2.state);
     setp3state(data.p3.state);
     setp4state(data.p4.state);
     setp5state(data.p5.state);
-        
+
     data.p1.state === "ready" ? setColor1("yellow") : setColor1("white");
     data.p2.state === "ready" ? setColor2("yellow") : setColor2("white");
     data.p3.state === "ready" ? setColor3("yellow") : setColor3("white");
@@ -126,49 +126,49 @@ const Waiting = (props) => {
   }
 
   // user 닉네임 세팅 함수
-  const SettingUser = (data)=>{
-      if (data.p1.nickname != "") {
-        if (data.p1.nickname == nickname) {
-          setpMyposition(1);
-        }
-        setp1(data.p1.nickname);
+  const SettingUser = (data) => {
+    if (data.p1.nickname != "") {
+      if (data.p1.nickname == nickname) {
+        setpMyposition(1);
       }
+      setp1(data.p1.nickname);
+    }
 
-      if (data.p2.nickname != "") {
-        if (data.p2.nickname == nickname) {
-          setpMyposition(2);
-        }
-        setp2(data.p2.nickname);
+    if (data.p2.nickname != "") {
+      if (data.p2.nickname == nickname) {
+        setpMyposition(2);
       }
-      if (data.p3.nickname != "") {
-        if (data.p3.nickname == nickname) {
-          setpMyposition(3);
-        }
-        setp3(data.p3.nickname);
+      setp2(data.p2.nickname);
+    }
+    if (data.p3.nickname != "") {
+      if (data.p3.nickname == nickname) {
+        setpMyposition(3);
       }
-      if (data.p4.nickname != "") {
-        if (data.p4.nickname == nickname) {
-          setpMyposition(4);
-        }
-        setp4(data.p4.nickname);
+      setp3(data.p3.nickname);
+    }
+    if (data.p4.nickname != "") {
+      if (data.p4.nickname == nickname) {
+        setpMyposition(4);
       }
-      if (data.p5.nickname != "") {
-        if (data.p5.nickname == nickname) {
-          setpMyposition(5);
-        }
-        setp5(data.p5.nickname);
+      setp4(data.p4.nickname);
+    }
+    if (data.p5.nickname != "") {
+      if (data.p5.nickname == nickname) {
+        setpMyposition(5);
       }
+      setp5(data.p5.nickname);
+    }
   }
 
   // 3명 이상의 user 가 모두 ready 인지 체크 함수
-  const CheckAllready = (data)=>{
+  const CheckAllready = (data) => {
 
-    let count =0;
-    
+    let count = 0;
+
     if (data.p1.nickname != "") {
       if (data.p1.state != "ready") {
         return false;
-      }else{
+      } else {
         count++;
       }
     }
@@ -176,7 +176,7 @@ const Waiting = (props) => {
     if (data.p2.nickname != "") {
       if (data.p2.state != "ready") {
         return false;
-      }else{
+      } else {
         count++;
       }
     }
@@ -184,7 +184,7 @@ const Waiting = (props) => {
     if (data.p3.nickname != "") {
       if (data.p3.state != "ready") {
         return false;
-      }else{
+      } else {
         count++;
       }
     }
@@ -192,7 +192,7 @@ const Waiting = (props) => {
     if (data.p4.nickname != "") {
       if (data.p4.state != "ready") {
         return false;
-      }else{
+      } else {
         count++;
       }
     }
@@ -200,20 +200,35 @@ const Waiting = (props) => {
     if (data.p5.nickname != "") {
       if (data.p5.state != "ready") {
         return false;
-      }else{
+      } else {
         count++;
       }
     }
 
-    //console.log(count);
-    
-    if(count > 2){
+    console.log(count);
+
+    if (count > 2) {
       return true;
-    }else {
+    } else {
       return false;
     }
   }
 
+
+  //카운트다운 후 게임 화면으로 전환
+  const [count, setCount]= useState(0);
+  function Counter(){
+
+    setInterval(()=>{
+      setCount(count - 1);
+      console.log(count)
+      if(count == 0){
+        start();
+      }
+    },5000);
+
+    return console.log(count);
+  }
   useEffect(() => {
 
     // props 의 방입장 코드 및 닉네임 설정
@@ -222,29 +237,33 @@ const Waiting = (props) => {
 
     // user 입장마다 이벤트 발생
     socket.on("add", function (data) {
-      if(data != null){
+      if (data != null) {
         SettingUser(data);
         SettingState(data);
       }
     });
-    
+
     // user 상태 변경시 마다 이벤트 발생
     socket.on("state", function (data) {
-      if(data != null){
+      if (data != null) {
         SettingState(data);
-        if( CheckAllready(data) == true ){
+        if (CheckAllready(data) == true) {
           console.log("All user ready");
-        }else {
+          Counter();
+        } else {
           console.log("All user not ready");
+          clearInterval(count);
         }
       }
     });
-    
+
+
     // waiting 화면 들어올 시 , user 등록 요청
-    if(enterCode != "" && nickname != ""){
+    if (enterCode != "" && nickname != "") {
       socket.emit("add", [enterCode, nickname]);
     }
   }, [nickname, enterCode]);
+
 
   return (
     <>
@@ -258,7 +277,6 @@ const Waiting = (props) => {
         </Content>
         <PlayerContainer>
           <Player>
-            <StartBtn onClick={startClick}>시작하기</StartBtn>
             <ReadyBtn onClick={onReady}>준비하기</ReadyBtn>
           </Player>
           <Player color={color1}>
